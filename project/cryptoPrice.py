5a9ecabf4fccc911c4a2b14044b544f791ca60e3
ShuoPluss <shuomathplus@outlook.com>
2022-06-09 10:40:52 -0500

    Create cryptoPrice.py

---
+++ project/cryptoPrice.py
@@ -0,0 +1,39 @@
+# in this project I will summarize the Bitcoin price in the past two years
+
+import pandas as pd
+import yfinance as yf
+import datetime
+from datetime import date, timedelta
+import plotly.graph_objects as go
+from autots import AutoTS
+
+today = date.today()
+
+d1 = today.strftime("%Y-%m-%d")
+end_date = d1
+d2 = date.today() - timedelta(days=730) #last two years
+d2 = d2.strftime("%Y-%m-%d")
+start_date = d2
+
+data = yf.download('BTC-USD', 
+                      start=start_date, 
+                      end=end_date, 
+                      progress=False)
+
+
+data["Date"] = data.index
+data = data[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]]
+data.reset_index(drop=True, inplace=True)
+print(data.tail())
+
+
+figure = go.Figure(data=[go.Candlestick(x=data["Date"],
+                                        open=data["Open"], 
+                                        high=data["High"],
+                                        low=data["Low"], 
+                                        close=data["Close"])])
+figure.update_layout(title = "Bitcoin Price Analysis", 
+                     xaxis_rangeslider_visible=False)
+figure.show()
+correlation = data.corr()
+print(correlation["Close"].sort_values(ascending=False))
